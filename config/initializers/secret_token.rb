0d4cfad64ffdaa85f9458cd30fb1c94e29ac9f83
djon3s <hack@riseup.net>
2013-06-01 04:15:39 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archivehero::Application.config.secret_token = 'e9e3b027335abf30571d134cc9f73087e9f8199f9e62b1c845f5c45558ee9da2af45f26d8d4264846fd2cd2f9d37753849f71439d89401af1d69d368db740967'
